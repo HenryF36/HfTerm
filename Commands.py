d935355ba14505a76e428439053da5e3e9da77c1
HenryF36 <Henryfabinski36@outlook.com>
2024-12-08 10:55:02 -0500

    Added About

--- Commands.py
+++ Commands.py
@@ -3,6 +3,6 @@ def Comm(Usrn, Com):
     if Com == "":
          print("")
     elif Com == "About":
-        print("")
+        print("HFterm is a terminal application in dev.\nIt is on the Github Repo HfTerm.\nSee License for more details")
     else:
         print(f"The command \"{Com}\" is not a command for {Usrn}.\n")
